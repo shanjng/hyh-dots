958617eb76192683387be65954f3a05cbfc18f5a
shanjng <shanjng@umich.edu>
2019-07-30 10:12:36 +0200

    merging edits from shan's computer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,6 +25,10 @@ export class HomeComponent implements OnInit {
     localStorage.setItem('navBar', JSON.stringify(this.navItems));
   }
 
+  browse()
+{
+  this.router.navigate(['nav/browse']);
+}
   login(){
 
     this.router.navigate(['nav/login']);
