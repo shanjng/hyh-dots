a8e16e5d4c4a56359e2af03c05d13d45501f08e6
benji <bennie.h.swanepoel@gmail.com>
2019-08-05 12:16:19 +0200

    local storage to browser

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -1,46 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import {NavService} from '../../services/nav.service';
-import { NavBar } from '../../nav-bar'
-import {UserService} from '../../services/user.service';
-import {User} from '../../models/twitterUser.model'
+import { Component, OnInit } from "@angular/core";
+import { NavService } from "../../services/nav.service";
+import { NavBar } from "../../nav-bar";
+import { UserService } from "../../services/user.service";
+import { TwitterUser } from "../../models/twitterUser.model";
 
 @Component({
-  selector: 'app-browse',
-  templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.scss']
+  selector: "app-browse",
+  templateUrl: "./browse.component.html",
+  styleUrls: ["./browse.component.scss"]
 })
 export class BrowseComponent implements OnInit {
-  public navBar = new NavBar()
+  public navBar = new NavBar();
   users: any;
-  selectedUser: User;
-  onSelect(user: User): void {
-    
-    
-   this.selectedUser = user;
-  }
 
-  constructor(private navService: NavService, private userService: UserService) {}
+  selectedUser: TwitterUser;
+  onSelect(user: TwitterUser): void {
+    this.selectedUser = user;
+    localStorage.setItem("TwitterUser", this.selectedUser.id_str);  }
+
+  constructor(
+    private navService: NavService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
-    if (localStorage.getItem('nav')==='true'){
-      this.navService.reloadNav().then(res=>{
-        console.log(res);
-  
-      }).catch(err=>{
-        console.log(err);
-      }) 
+    if (localStorage.getItem("nav") === "true") {
+      this.navService
+        .reloadNav()
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-      this.userService.getUsers().then(res=>{
+    this.userService
+      .getUsers()
+      .then(res => {
         console.log(res);
-        this.users=res;
-      }).catch(err=>{
-        console.log(err);
+        this.users = res;
       })
-      
-
+      .catch(err => {
+        console.log(err);
+      });
   }
-
-
 }
-
-
--- src/app/models/twitterUser.model.ts
+++ src/app/models/twitterUser.model.ts
@@ -1,4 +1,4 @@
-export class User {
+export class TwitterUser {
     public id: number;
     public id_str: string;
     public name: string;
